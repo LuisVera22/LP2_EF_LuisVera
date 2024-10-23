aad0c8d7d0bce5f7494bf5f685fa986dfd54e5b8
Malena Vera <luis22vera03@gmail.com>
2024-10-23 18:56:39 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/evaluacion/LP2_EF_LuisVera/Lp2EfLuisVeraApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.LP2_EF_LuisVera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfLuisVeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfLuisVeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion/LP2_EF_LuisVera/Lp2EfLuisVeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.LP2_EF_LuisVera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2EfLuisVeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
